0f8ef6af65c0173c4d96dea800616f817de5845a
BrennanL <lesuer.brennan@gmail.com>
2021-04-11 17:32:54 -0400

    Pubmed author emails
    The scraper is still grabbing duplicate pmcids which is leading to duplicate emails, but they are at least accesible now

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-from urllib.request import urlopen
+import requests
 from bs4 import BeautifulSoup
 import argparse
 
@@ -10,19 +10,21 @@ parser.add_argument("--stop", type=int, help="The page that the scraper will fin
 args = parser.parse_args()
 
 # email_list.txt is the final file shipped for import
-email_list = open("email_list.txt", "w+")
+biorxiv_email_list = open("biorxiv_email_list.txt", "w+")
+pubmed_email_list = open("pubmed_email_list.txt", "w+")
 
-def scrape_biorxiv(span, url):
+##
+# Scraper functions
+##
+def scrape_biorxiv_article(span, url):
     # add into list newly built urls that go to content pages
     string = span.text
     # split is using an EM DASH 
     article_url = url + string.split(" —")[0] + "v1.article-info"
 
     # Get markup and read it into BS
-    page = urlopen(article_url)
-    html_bytes = page.read()
-    html_content = html_bytes.decode("utf-8")
-    soup_content = BeautifulSoup(html_content, "html.parser")
+    page = requests.get(article_url).text
+    soup_content = BeautifulSoup(page, "html.parser")
 
     # append the corresponding author name before the email 
     name_tag = soup_content.find("span", "name")
@@ -33,11 +35,30 @@ def scrape_biorxiv(span, url):
     email_string = email_tag.string.extract()
     author_email = email_string.replace("{at}", "@")
 
-    email_list.write(author_email + " " + author_name + "\n")
+    biorxiv_email_list.write(author_email + " " + author_name + "\n")
 
-def scrap_pubmed(url):
-    print(url)
+def scrape_pubmed_article(url):
+    # Get soup content of article page
+    page = requests.get(url).text
+    soup_content = BeautifulSoup(page, "html.parser")
 
+    # the PMCID of the article is a link to a page containing the author info
+    article_ids = soup_content.findAll("a", {"data-ga-action" : "PMCID"})
+    for link in range(len(article_ids)):
+        article_pmc = article_ids[link]["href"]
+        convert_link = article_pmc.replace("http", "https")
+        page = requests.get(convert_link).text
+        soup_content = BeautifulSoup(page, "html.parser")
+    
+        # The source code of the article has author emails in reverse
+        # seems like they tried to confuse anyone looking to scrape them?
+        emails_in_reverse = soup_content.findAll("a", {"class" : "oemail"})
+        for email_tag in range(len(emails_in_reverse)):
+            pubmed_email_list.write(emails_in_reverse[email_tag]["data-email"][::-1] + "\n")
+
+## 
+# Main page logic
+##
 # determine which site the user wanted to scrape first
 if (args.site == "biorxiv"):
     print("\nBiorxiv preprints related to Covid-19 and Sars-Cov-2")
@@ -47,10 +68,8 @@ if (args.site == "biorxiv"):
         main_url = "https://connect.biorxiv.org/relate/content/181?page="+str(num)
 
         # Give the HTML content Beautiful Soup
-        page = urlopen(main_url)
-        html_bytes = page.read()
-        html_content = html_bytes.decode("utf-8")
-        soup_content = BeautifulSoup(html_content, "html.parser")
+        page = requests.get(main_url).text
+        soup_content = BeautifulSoup(page, "html.parser")
 
         # find url extensions for each article
         get_article_metadata = soup_content.find_all("span", "highwire-cite-metadata-journal")
@@ -62,9 +81,9 @@ if (args.site == "biorxiv"):
         for span in get_article_metadata:
             link = span.a['href']
             if "medrx" in link:
-                scrape_biorxiv(span, "https://www.medrxiv.org/content/")
+                scrape_biorxiv_article(span, "https://www.medrxiv.org/content/")
             elif "biorx" in link:
-                scrape_biorxiv(span, "https://www.biorxiv.org/content/")
+                scrape_biorxiv_article(span, "https://www.biorxiv.org/content/")
             else:
                 print("WARNING -  " + link + " was not scraped because it's site needs to be added to the biorxiv scraper")
         print("done.")
@@ -75,10 +94,8 @@ elif(args.site == "pubmed"):
         pubmed_url = "https://pubmed.ncbi.nlm.nih.gov/?term=covid-19&page="+str(num)
 
         # Give the HTML content to Beautiful Soup
-        page = urlopen(pubmed_url)
-        html_bytes = page.read()
-        html_content = html_bytes.decode("utf-8")
-        soup_content = BeautifulSoup(html_content, "html.parser")
+        page = requests.get(pubmed_url).text
+        soup_content = BeautifulSoup(page, "html.parser")
 
         # Get each article element off the page
         get_article_metadata = soup_content.find_all("a","docsum-title")
@@ -88,11 +105,11 @@ elif(args.site == "pubmed"):
         print("scraping..")
         # Use the article ID's to scrape their author names
         for article in range(len(get_article_metadata)):
-            link = get_article_metadata[article]["href"]
-            scrap_pubmed("https://pubmed.ncbi.nlm.nih.gov/" + link)
-
+            article_id = get_article_metadata[article]["href"]
+            scrape_pubmed_article("https://pubmed.ncbi.nlm.nih.gov" + article_id)
 else:
     print("The site " + args.site + " is not yet supported by the scraper.")
 
-email_list.close()
-print("\nrun command 'cat email_list.txt' to see results")
+biorxiv_email_list.close()
+pubmed_email_list.close()
+print("\nrun command 'cat <site>_email_list.txt' to see results")
